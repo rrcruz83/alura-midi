b62777e99e27317ca00070376dd0dec1bc0e3362
rrcruz83 <rrcruz83@gmail.com>
2023-01-10 16:37:16 -0300

    Ajustes para buscar o elemento através de uma lista de elementos utilizando o querySelectorAll

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
 function tocaSomPom() {
     document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
-
-function tocaSomClap() {
-    document.querySelector('#som_tecla_clap').play();
-}
-
-document.querySelector('.tecla_clap').onclick = tocaSomClap;
+listaDeTeclas[0].onclick = tocaSomPom;
