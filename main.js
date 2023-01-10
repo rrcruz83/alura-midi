91385c10920b67210f9be60da59aa985fbca697c
rrcruz83 <rrcruz83@gmail.com>
2023-01-10 15:23:31 -0300

    Criação da função tocaSomPom e tocaSomClap que executa o id correspondente ao ser chamado através do toque onclik

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
-const botao_pom = document.querySelector('.tecla_pom') 
+function tocaSomPom() {
+    document.querySelector('#som_tecla_pom').play();
+}
 
-console.log(botao_pom)
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+function tocaSomClap() {
+    document.querySelector('#som_tecla_clap').play();
+}
+
+document.querySelector('.tecla_clap').onclick = tocaSomClap;
