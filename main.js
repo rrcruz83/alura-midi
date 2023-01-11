042e6261c336f8dab30178bd78bccff3ee5bf65b
rrcruz83 <rrcruz83@gmail.com>
2023-01-11 14:52:16 -0300

    Criacão de uma estrutura de repetição para imprimir no campo de input o valor digitado na tela

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
 const listaDeTeclas = document.querySelectorAll('.tecla');
+const listaDeAudios = document.querySelectorAll('audio');
 
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
-listaDeTeclas[0].onclick = tocaSomPom;
+//for ([inicialização]; [condição]; [expressão final]) { declaração }
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+    const tecla = listaDeTeclas[contador];
+    //console.log(tecla);
+    const instrumento = tecla.classList[1];
+    //console.log(instrumento);
+
+    //Template string `string text ${expression} string text`
+    const idAudio = `#som_${instrumento}`;
+    
+    //console.log(idAudio);
+
+    listaDeTeclas[contador].onclick = function () {
+        tocaSom(idAudio);
+        console.log('Esse é o som do ' + instrumento.slice(6));//.slice(startIndex[, endIndex])  
+    };
+}
